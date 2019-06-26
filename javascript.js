1e9905aaf75803dd3c62de85fd5e1f6ae847f49c
Silky Silky <747916@student.tor.loc>
2019-06-26 15:03:00 -0400

    3.00pm commit

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 function waitingFn(timeinMs){
-    const futureTime = Date.now() =timeInMs;
+    const futureTime = Date.now() +timeInMs;
 
     while(futureTime) >Date.now()){
         //waiting
     }
 }
+
 waitingFn(3000);
 
 console.log("Function call has just ended");
